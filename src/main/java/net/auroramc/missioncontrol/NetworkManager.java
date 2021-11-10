2f2121050d7b65b506b8ba5d290d7d48b2db187f
Block2Block <accounts@block2block.me>
2021-11-10 18:52:21 +0000

    Changed initial delay for updating statistics

--- src/main/java/net/auroramc/missioncontrol/NetworkManager.java
+++ src/main/java/net/auroramc/missioncontrol/NetworkManager.java
@@ -193,9 +193,9 @@ public class NetworkManager {
         scheduler.scheduleWithFixedDelay(new RequestPlayerCountUpdateRunnable(), 5, 5, TimeUnit.MINUTES);
 
         //Statistics updater
-        scheduler.scheduleWithFixedDelay(new StatUpdateRunnable(StatUpdateRunnable.StatisticPeriod.DAILY), 1, 1, TimeUnit.MINUTES);
-        scheduler.scheduleWithFixedDelay(new StatUpdateRunnable(StatUpdateRunnable.StatisticPeriod.WEEKLY), 10, 10, TimeUnit.MINUTES);
-        scheduler.scheduleWithFixedDelay(new StatUpdateRunnable(StatUpdateRunnable.StatisticPeriod.ALLTIME), 1, 1, TimeUnit.DAYS);
+        scheduler.scheduleWithFixedDelay(new StatUpdateRunnable(StatUpdateRunnable.StatisticPeriod.DAILY), 0, 1, TimeUnit.MINUTES);
+        scheduler.scheduleWithFixedDelay(new StatUpdateRunnable(StatUpdateRunnable.StatisticPeriod.WEEKLY), 0, 10, TimeUnit.MINUTES);
+        scheduler.scheduleWithFixedDelay(new StatUpdateRunnable(StatUpdateRunnable.StatisticPeriod.ALLTIME), 0, 1, TimeUnit.DAYS);
 
         done();
     }
