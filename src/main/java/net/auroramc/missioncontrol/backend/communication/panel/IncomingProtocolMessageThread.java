b4c346d0c3debc9283b278ec789ed7d91fc23620
Block2Block <accounts@block2block.me>
2021-11-20 20:47:37 +0000

    Test panel message

---
+++ src/main/java/net/auroramc/missioncontrol/backend/communication/panel/IncomingProtocolMessageThread.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.missioncontrol.backend.communication.panel;
+
+import net.auroramc.missioncontrol.MissionControl;
+import net.auroramc.missioncontrol.backend.communication.ProxyMessageHandler;
+import net.auroramc.proxy.api.backend.communication.ProtocolMessage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.logging.Level;
+
+public class IncomingProtocolMessageThread extends Thread {
+
+    private int port;
+    private boolean listening;
+    private ServerSocket socket;
+
+    public IncomingProtocolMessageThread(int port) {
+        this.port = port;
+        this.setName("Incoming Panel Protocol Messages Thread");
+        this.setDaemon(true);
+        listening = true;
+    }
+
+    @Override
+    public void run() {
+        try (ServerSocket socket = new ServerSocket(port)) {
+            this.socket = socket;
+            while (listening) {
+                Socket connection = socket.accept();
+                MissionControl.getLogger().log(Level.FINEST, "Accepted connection from panel.");
+                byte[] resultBuff = new byte[0];
+                byte[] buff = new byte[1024];
+                int k = -1;
+                while((k = connection.getInputStream().read(buff, 0, buff.length)) > -1) {
+                    byte[] tbuff = new byte[resultBuff.length + k]; // temp buffer size = bytes already read + bytes last read
+                    System.arraycopy(resultBuff, 0, tbuff, 0, resultBuff.length); // copy previous bytes
+                    System.arraycopy(buff, 0, tbuff, resultBuff.length, k);  // copy current lot
+                    resultBuff = tbuff; // call the temp buffer as your result buff
+                }
+                String message = new String(resultBuff);
+                try {
+                    connection.getOutputStream().write(PanelMessageHandler.onMessage(message).getBytes());
+                    connection.getOutputStream().flush();
+                } catch (Exception e) {
+                    MissionControl.getLogger().log(Level.WARNING,"An error occurred when attempting to handle a panel message. Stack trace: ", e);
+                }
+            }
+        } catch (SocketException e) {
+            listening = false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void shutdown() {
+        listening = false;
+        try {
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/net/auroramc/missioncontrol/backend/communication/panel/PanelMessageHandler.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2021 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.missioncontrol.backend.communication.panel;
+
+import net.auroramc.missioncontrol.MissionControl;
+import net.auroramc.missioncontrol.NetworkManager;
+import net.auroramc.missioncontrol.NetworkMonitorRunnable;
+import net.auroramc.missioncontrol.backend.util.Game;
+import net.auroramc.missioncontrol.backend.util.Module;
+import net.auroramc.missioncontrol.entities.ServerInfo;
+
+import java.util.*;
+
+public class PanelMessageHandler {
+
+    public static String onMessage(String message) {
+        List<String> args = new ArrayList<>(Arrays.asList(message.split(";")));
+        String command = args.remove(0);
+        switch (command.toLowerCase()) {
+            case "createserver": {
+                if (args.size() == 2) {
+                    ServerInfo.Network network;
+                    try {
+                        network = ServerInfo.Network.valueOf(args.get(0));
+                    } catch (IllegalArgumentException e) {
+                        List<String> networks = new ArrayList<>();
+                        for (ServerInfo.Network network2 : ServerInfo.Network.values()) {
+                            networks.add(network2.name());
+                        }
+                        return "An invalid network was inputted.";
+                    }
+
+                    Game game;
+                    try {
+                        game = Game.valueOf(args.get(1));
+                    } catch (IllegalArgumentException e) {
+                        List<String> games = new ArrayList<>();
+                        for (Game game2 : Game.values()) {
+                            games.add(game2.name());
+                        }
+                        return "An invalid game was inputted.";
+                    }
+
+                    String serverName = null;
+                    List<String> extraArgs = null;
+
+                    if (!args.get(2).equalsIgnoreCase("")) {
+                        serverName = args.get(2);
+                    }
+
+                    if (MissionControl.getServers().get(network).containsKey(serverName)) {
+                        return "That server already exists on that network.";
+                    }
+
+                    if (!args.get(3).equalsIgnoreCase("")) {
+                        extraArgs = Arrays.asList(args.get(3).split(" "));
+                    }
+                    if (extraArgs != null && network == ServerInfo.Network.TEST) {
+                        Map<Module, Integer> moduleBuilds = new HashMap<>();
+                        Map<Module, String> moduleBranches = new HashMap<>();
+                        for (String arg : extraArgs) {
+                            String[] moduleArg = arg.split("=");
+                            String[] branchArg = moduleArg[1].split(":");
+
+                            String branch = branchArg[0];
+                            int build = Integer.parseInt(branchArg[1]);
+                            Module module;
+                            try {
+                                module = Module.valueOf(moduleArg[0].toUpperCase());
+                            } catch (IllegalArgumentException e) {
+                                return "Module '" + moduleArg[0] + "' does not exist.";
+                            }
+
+                            if (!game.getModules().contains(module)) {
+                                continue;
+                            }
+
+                            if (!MissionControl.getJenkinsManager().branchExists(module, branch)) {
+                                return "The branch '" + branch + "' does not exist for this module. If it should exist, make sure that the CI job has been executed before trying again.";
+                            }
+
+                            if (!MissionControl.getJenkinsManager().buildExists(module, branch, build)) {
+                                return "The build '" + build + "' does not exist for this module/branch. If it should exist, make sure that the CI job has been executed before trying again.";
+                            }
+
+                            moduleBranches.put(module, branch);
+                            moduleBuilds.put(module, build);
+                        }
+
+                        if (moduleBranches.size() != game.getModules().size()) {
+                            return "You have not specified the branch and build number for all modules. Aborting.";
+                        }
+
+                        ServerInfo info = NetworkManager.createServer(serverName, game, true, network, moduleBuilds.getOrDefault(Module.CORE, -1), moduleBranches.get(Module.CORE), moduleBuilds.getOrDefault(Module.LOBBY, -1), moduleBranches.get(Module.LOBBY), moduleBuilds.getOrDefault(Module.BUILD, -1), moduleBranches.get(Module.BUILD), moduleBuilds.getOrDefault(Module.GAME, -1), moduleBranches.get(Module.GAME), moduleBuilds.getOrDefault(Module.ENGINE, -1), moduleBranches.get(Module.ENGINE));
+                        if (info != null) {
+                            return "Success";
+                        } else {
+                            return "Server creation failed.";
+                        }
+                    } else {
+                        if (network == ServerInfo.Network.TEST) {
+                            return "You must provide the server name and branch/build details for each module the server will deploy with when creating a server on the test network.";
+                        }
+
+                        if (serverName == null) {
+                            int id = NetworkMonitorRunnable.findLowestAvailableServerID(game, network);
+                            serverName = game.getServerCode() + "-" + id;
+                        } else if (MissionControl.getServers().get(network).containsKey(serverName)) {
+                            return "That server already exists on that network.";
+                        }
+
+                        ServerInfo info = NetworkManager.createServer(serverName, game, true, network);
+                        if (info != null) {
+                            return "Success";
+                        } else {
+                            return "Server creation failed.";
+                        }
+                    }
+                } else {
+                    return "The command executed does not have correct arguments. Please try again.";
+                }
+            }
+        }
+        return "The command executed was not recognised by Mission Control. Please try again.";
+    }
+
+}
