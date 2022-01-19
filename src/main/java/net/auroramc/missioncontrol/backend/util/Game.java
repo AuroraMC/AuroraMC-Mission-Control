ab2bec1627abbdb427070399686b6fb425bd632a
Block2Block <accounts@block2block.me>
2022-01-19 15:57:50 +0000

    Added some stuff to server type so the server knows its max capacity and whether to heavily enforce the cap

--- src/main/java/net/auroramc/missioncontrol/backend/util/Game.java
+++ src/main/java/net/auroramc/missioncontrol/backend/util/Game.java
@@ -16,12 +16,12 @@ import java.util.List;
  */
 public enum Game {
 
-    LOBBY(80, "Lobby", new JSONObject().put("type", "lobby").put("game", "LOBBY"), MemoryAllocation.LOBBY, Collections.singletonList(Module.LOBBY)),
-    EVENT(100, "Event", new JSONObject().put("type", "game").put("game", "EVENT").put("event", true).put("rotation", new JSONArray().put("EVENT")), MemoryAllocation.EVENT, Arrays.asList(Module.ENGINE, Module.GAME, Module.EVENT)),
-    CRYSTAL_QUEST(10, "CQ", new JSONObject().put("type", "game").put("game", "CRYSTAL_QUEST").put("rotation", new JSONArray().put("CRYSTAL_QUEST")), MemoryAllocation.GAME, Arrays.asList(Module.ENGINE, Module.GAME)),
-    MIXED_ARCADE(16, "Arcade", new JSONObject().put("type", "game").put("game", "MIXED_ARCADE").put("rotation", new JSONArray().put("SPLEEF").put("RUNNER")), MemoryAllocation.GAME, Arrays.asList(Module.ENGINE, Module.GAME)),
-    BUILD(32, "Build", new JSONObject().put("type", "build").put("game", "BUILD"), MemoryAllocation.GAME, Collections.singletonList(Module.BUILD)),
-    STAFF(80, "Staff", new JSONObject().put("type", "staff").put("game", "STAFF").put("rotation", new JSONArray()), MemoryAllocation.LOBBY, Arrays.asList(Module.ENGINE, Module.GAME));
+    LOBBY(80, "Lobby", new JSONObject().put("type", "lobby").put("game", "LOBBY").put("max_players", 80), MemoryAllocation.LOBBY, Collections.singletonList(Module.LOBBY)),
+    EVENT(100, "Event", new JSONObject().put("type", "game").put("game", "EVENT").put("event", true).put("rotation", new JSONArray().put("EVENT")).put("max_players", 100), MemoryAllocation.EVENT, Arrays.asList(Module.ENGINE, Module.GAME, Module.EVENT)),
+    CRYSTAL_QUEST(10, "CQ", new JSONObject().put("type", "game").put("game", "CRYSTAL_QUEST").put("rotation", new JSONArray().put("CRYSTAL_QUEST")).put("max_players", 10).put("enforce_limit", true), MemoryAllocation.GAME, Arrays.asList(Module.ENGINE, Module.GAME)),
+    MIXED_ARCADE(16, "Arcade", new JSONObject().put("type", "game").put("game", "MIXED_ARCADE").put("rotation", new JSONArray().put("SPLEEF").put("RUNNER")).put("max_players", 16), MemoryAllocation.GAME, Arrays.asList(Module.ENGINE, Module.GAME)),
+    BUILD(32, "Build", new JSONObject().put("type", "build").put("game", "BUILD").put("max_players", 32), MemoryAllocation.GAME, Collections.singletonList(Module.BUILD)),
+    STAFF(80, "Staff", new JSONObject().put("type", "staff").put("game", "STAFF").put("rotation", new JSONArray()).put("max_players", 80), MemoryAllocation.LOBBY, Arrays.asList(Module.ENGINE, Module.GAME));
 
     private final int maxPlayers;
     private final String serverCode;
