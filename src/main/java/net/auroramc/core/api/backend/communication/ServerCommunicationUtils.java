60fc334c17f06adb22fed7cc78bc20a5f6e7a7fc
Block2Block <accounts@block2block.me>
2022-01-27 17:22:06 +0000

    Hopefully this will stop breaking when there is a corrupted stream

--- src/main/java/net/auroramc/core/api/backend/communication/ServerCommunicationUtils.java
+++ src/main/java/net/auroramc/core/api/backend/communication/ServerCommunicationUtils.java
@@ -39,6 +39,31 @@ public class ServerCommunicationUtils {
             } catch (Exception e) {
                 if (message.getProtocol() != Protocol.UPDATE_PLAYER_COUNT || !NetworkManager.isUpdate()) {
                     MissionControl.getLogger().log(Level.WARNING, "An error occurred when attempting to contact server " + info.getName() + " on network " + info.getNetwork().name() + ". Stack Trace:", e);
+                    return sendMessage(message, network, 1);
+                }
+                return null;
+            }
+        }
+        return null;
+    }
+    public static UUID sendMessage(ProtocolMessage message, ServerInfo.Network network, int level) {
+        ServerInfo info = MissionControl.getServers().get(network).get(message.getDestination());
+        if (info != null) {
+            message.setServer(info.getName());
+            message.setAuthenticationKey(info.getAuthKey());
+            message.setNetwork(info.getNetwork().name());
+            try (Socket socket = new Socket(info.getIp(), info.getProtocolPort())) {
+                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                outputStream.writeObject(message);
+                outputStream.flush();
+                return message.getUuid();
+            } catch (Exception e) {
+                if (message.getProtocol() != Protocol.UPDATE_PLAYER_COUNT || !NetworkManager.isUpdate()) {
+                    if (level > 4) {
+                        MissionControl.getLogger().log(Level.WARNING, "An error occurred when attempting to contact server " + info.getName() + " on network " + info.getNetwork().name() + ". Stack Trace:", e);
+                        return null;
+                    }
+                    return sendMessage(message, network, level + 1);
                 }
                 return null;
             }
