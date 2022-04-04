fc33e87794de604674ff81876ea3dddef33d1c39
Block2Block <accounts@block2block.me>
2022-04-05 00:30:14 +0100

    Add FFA to game rotation

--- src/main/java/net/auroramc/missioncontrol/backend/util/Game.java
+++ src/main/java/net/auroramc/missioncontrol/backend/util/Game.java
@@ -19,7 +19,7 @@ public enum Game {
     LOBBY(80, "Lobby", new JSONObject().put("type", "lobby").put("game", "LOBBY").put("max_players", 80), MemoryAllocation.LOBBY, Collections.singletonList(Module.LOBBY)),
     EVENT(100, "Event", new JSONObject().put("type", "game").put("game", "EVENT").put("event", true).put("rotation", new JSONArray().put("EVENT")).put("max_players", 100).put("min_players", 50), MemoryAllocation.EVENT, Arrays.asList(Module.ENGINE, Module.GAME, Module.EVENT)),
     CRYSTAL_QUEST(10, "CQ", new JSONObject().put("type", "game").put("game", "CRYSTAL_QUEST").put("rotation", new JSONArray().put("CRYSTAL_QUEST")).put("max_players", 10).put("min_players", 8).put("enforce_limit", true), MemoryAllocation.GAME, Arrays.asList(Module.ENGINE, Module.GAME)),
-    ARCADE_MODE(16, "Arcade", new JSONObject().put("type", "game").put("game", "ARCADE_MODE").put("rotation", new JSONArray().put("SPLEEF")).put("max_players", 16).put("min_players", 8), MemoryAllocation.GAME, Arrays.asList(Module.ENGINE, Module.GAME)),
+    ARCADE_MODE(16, "Arcade", new JSONObject().put("type", "game").put("game", "ARCADE_MODE").put("rotation", new JSONArray().put("SPLEEF").put("FFA")).put("max_players", 16).put("min_players", 8), MemoryAllocation.GAME, Arrays.asList(Module.ENGINE, Module.GAME)),
     BUILD(32, "Build", new JSONObject().put("type", "build").put("game", "BUILD").put("max_players", 32), MemoryAllocation.GAME, Collections.singletonList(Module.BUILD)),
     STAFF(80, "Staff", new JSONObject().put("type", "staff").put("game", "STAFF").put("rotation", new JSONArray()).put("max_players", 80).put("min_players", 40), MemoryAllocation.LOBBY, Arrays.asList(Module.ENGINE, Module.GAME));
 
