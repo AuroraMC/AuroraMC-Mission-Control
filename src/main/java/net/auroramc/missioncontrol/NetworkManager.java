56c5506ba0ec8ede46fedb492bfd9dc0687e1b78
Block2Block <accounts@block2block.me>
2022-04-05 22:52:31 +0100

    Specify proxy update

--- src/main/java/net/auroramc/missioncontrol/NetworkManager.java
+++ src/main/java/net/auroramc/missioncontrol/NetworkManager.java
@@ -334,7 +334,7 @@ public class NetworkManager {
             }
         }
 
-        logger.info("Update complete.");
+        logger.info("Proxy uSpdate complete.");
 
         //Update the MOTD to what it was before.
         for (ProxyInfo uuid : MissionControl.getProxies().values().stream().filter(proxyInfo -> proxyInfo.getNetwork() == network).collect(Collectors.toList())) {
