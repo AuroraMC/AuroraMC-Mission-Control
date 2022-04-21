b9aeb94ba7bd73f32ee5a0b3d234ce2ccd0eecea
Block2Block <accounts@block2block.me>
2022-04-21 23:09:35 +0300

    Players per game are now tracked properly

--- src/main/java/net/auroramc/missioncontrol/backend/runnables/StatUpdateRunnable.java
+++ src/main/java/net/auroramc/missioncontrol/backend/runnables/StatUpdateRunnable.java
@@ -9,6 +9,9 @@ import net.auroramc.missioncontrol.backend.util.Game;
 import net.auroramc.missioncontrol.backend.util.ServerType;
 import net.auroramc.missioncontrol.backend.util.Statistic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StatUpdateRunnable implements Runnable {
 
     private final StatisticPeriod frequency;
@@ -20,10 +23,24 @@ public class StatUpdateRunnable implements Runnable {
     @Override
     public void run() {
         long timestamp = System.currentTimeMillis();
+        Map<Game, Integer> totals = new HashMap<>();
+        Map<Game, Integer> avgPlayerPerGame = new HashMap<>();
         for (Statistic statistic : Statistic.values()) {
             if (statistic.isSplitIntoGame()) {
                 for (Game game : Game.values()) {
-                    MissionControl.getDbManager().insertStatistic(statistic, frequency, timestamp, statistic.getStat(game, frequency), game);
+                    int stat = statistic.getStat(game, frequency);
+                    if (statistic == Statistic.GAMES_STARTED) {
+                        totals.put(game, stat);
+                    } if (statistic == Statistic.PLAYERS_PER_GAME) {
+                        avgPlayerPerGame.put(game, totals.get(game) / stat);
+                        continue;
+                    }
+                    MissionControl.getDbManager().insertStatistic(statistic, frequency, timestamp, stat, game);
+                }
+                if (statistic == Statistic.PLAYERS_PER_GAME) {
+                    for (Map.Entry<Game, Integer> entry : avgPlayerPerGame.entrySet()) {
+                        MissionControl.getDbManager().insertStatistic(statistic, frequency, timestamp, entry.getValue(), entry.getKey());
+                    }
                 }
             } else {
                 MissionControl.getDbManager().insertStatistic(statistic, frequency, timestamp, statistic.getStat(null, frequency));
