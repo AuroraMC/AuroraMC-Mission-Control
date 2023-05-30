74c20a68da35767a7d987afd6feb4b30dc33881b
Block2Block <accounts@block2block.me>
2023-05-30 02:31:22 +0100

    Update copyright notice

--- src/main/java/net/auroramc/proxy/api/backend/communication/Protocol.java
+++ src/main/java/net/auroramc/proxy/api/backend/communication/Protocol.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2021 AuroraMC Ltd. All Rights Reserved.
+ * Copyright (c) 2021-2023 AuroraMC Ltd. All Rights Reserved.
+ *
+ * PRIVATE AND CONFIDENTIAL - Distribution and usage outside the scope of your job description is explicitly forbidden except in circumstances where a company director has expressly given written permission to do so.
  */
 
 package net.auroramc.proxy.api.backend.communication;
--- src/main/java/net/buycraft/plugin/shared/util/ReportBuilder.java
+++ src/main/java/net/buycraft/plugin/shared/util/ReportBuilder.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023 AuroraMC Ltd. All Rights Reserved.
+ *
+ * PRIVATE AND CONFIDENTIAL - Distribution and usage outside the scope of your job description is explicitly forbidden except in circumstances where a company director has expressly given written permission to do so.
+ */
+
 package net.buycraft.plugin.shared.util;
 
 import net.buycraft.plugin.IBuycraftPlatform;
