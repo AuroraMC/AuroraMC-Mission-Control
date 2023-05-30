edbfd2e23e6e3a17c116e685f08db46ae37dc184
Block2Block <accounts@block2block.me>
2023-05-30 02:31:22 +0100

    Update copyright notice

--- src/main/java/net/auroramc/missioncontrol/backend/store/packages/ranks/Elite.java
+++ src/main/java/net/auroramc/missioncontrol/backend/store/packages/ranks/Elite.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2022 AuroraMC Ltd. All Rights Reserved.
+ * Copyright (c) 2022-2023 AuroraMC Ltd. All Rights Reserved.
+ *
+ * PRIVATE AND CONFIDENTIAL - Distribution and usage outside the scope of your job description is explicitly forbidden except in circumstances where a company director has expressly given written permission to do so.
  */
 
 package net.auroramc.missioncontrol.backend.store.packages.ranks;
